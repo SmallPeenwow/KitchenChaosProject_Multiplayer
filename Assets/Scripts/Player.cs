e432b68f6db506bdcabaef34161fd912934b250c
SmallPeenwow <bowdencamryn@gmail.com>
2023-07-23 09:38:55 +0200

    Sync Player Movement, Client Vs Server Auth

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -56,6 +56,12 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
 
     private void Update()
     {
+        if (IsOwner)
+        {
+            return;
+        }
+
+        //HandleMovementServerAuth();
         HandleMovement();
         HandleInteractions();
     }
@@ -97,6 +103,64 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
             SetSelectedCounter(null);
         }
     }
+
+    // Asks the server to move
+    //private void HandleMovementServerAuth()
+    //{
+    //    Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
+    //    HandleMovementServerRpc(inputVector);
+    //}
+
+    // ***
+    //[ServerRpc(RequireOwnership = false)]
+    //private void HandleMovementServerRpc(Vector2 inputVector)
+    //{
+    //    Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+    //    float moveDistance = moveSpeed * Time.deltaTime;
+    //    float PlayerRadius = 0.7f;
+    //    float playerHeight = 2f;
+    //    bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, PlayerRadius, moveDir, moveDistance);
+
+    //    if (!canMove)
+    //    {
+    //        // Cannot move towards moveDir
+
+    //        // Attempt only X movement
+    //        Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
+    //        canMove = (moveDir.x < -0.5f || moveDir.x > +0.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, PlayerRadius, moveDirX, moveDistance);
+
+    //        if (canMove)
+    //        {
+    //            // Can move only on the X
+    //            moveDir = moveDirX;
+    //        }
+    //        else
+    //        {
+    //            // Cannot move only on the X
+
+    //            // Attempt only Z movement
+    //            Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
+    //            canMove = (moveDir.z < -0.5f || moveDir.z > +0.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, PlayerRadius, moveDirZ, moveDistance);
+
+    //            if (canMove)
+    //            {
+    //                // Can move only on the Z
+    //                moveDir = moveDirZ;
+    //            }
+    //        }
+    //    }
+
+    //    if (canMove)
+    //    {
+    //        transform.position += moveDir * moveDistance;
+    //    }
+
+    //    isWalking = moveDir != Vector3.zero;
+
+    //    float rotateSpeed = 10f;
+    //    transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    //}
 
     private void HandleMovement()
     {
