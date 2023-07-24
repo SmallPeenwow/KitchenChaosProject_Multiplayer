508ead4ebec2737f2981bda2b782cbb19033713f
SmallPeenwow <bowdencamryn@gmail.com>
2023-07-24 18:25:30 +0200

    Sync Pick Up Objects Done

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -266,4 +266,9 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
     {
         return kitchenObject != null;
     }
+
+    public NetworkObject GetNetworkObject()
+    {
+        return NetworkObject;
+    }
 }
