1f013e66634543488a61621a6ccab81b1d93cbcc
SmallPeenwow <bowdencamryn@gmail.com>
2023-09-19 22:04:53 +0200

    Connection Scene Flow Done

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -1,3 +1,4 @@
+using Unity.Netcode;
 using UnityEngine.SceneManagement;
 
 public static class Loader
@@ -7,6 +8,8 @@ public static class Loader
         MainMenuScene,
         GameScene,
         LoadingScene,
+        LobbyScene,
+        CharacterSelectScene,
     }
 
     private static Scene targetScene;
@@ -16,6 +19,11 @@ public static class Loader
         Loader.targetScene = targetScene;
 
         SceneManager.LoadScene(Scene.LoadingScene.ToString());
+    }
+
+    public static void LoadNetwork(Scene targetScene)
+    {
+        NetworkManager.Singleton.SceneManager.LoadScene(targetScene.ToString(), LoadSceneMode.Single);
     }
 
     public static void LoaderCallback()
--- Assets/Scripts/UI/HostDisconnectUI.cs
+++ Assets/Scripts/UI/HostDisconnectUI.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,6 +5,16 @@ using UnityEngine.UI;
 public class HostDisconnectUI : MonoBehaviour
 {
     [SerializeField] private Button playAgainButton;
+
+    private void Awake()
+    {
+        playAgainButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.Shutdown();
+
+            Loader.Load(Loader.Scene.MainMenuScene);
+        });
+    }
 
     private void Start()
     {
